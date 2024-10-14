09ed7c4a057368c3e186cb934b0b84f91b4ab927
emrecivann <emrcivan@icloud.com>
2024-10-14 22:28:57 +0300

    first commit

---
+++ libft.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecivan <ecivan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 19:33:28 by ecivan            #+#    #+#             */
+/*   Updated: 2024/10/14 19:36:36 by ecivan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	isalpha(int c);
+int	isdigit(int c);
+int	isalnum(int c);
+int	isascii(int c);
+int	isprint(int c);
+size_t	strlen(const char *s);
